8346a5a6628f198958fc36cd335779286505db12
wangxing <18912540921@163.com>
2017-11-05 19:54:58 +0800

    edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,9 +18,13 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
   end
 
   def delete
