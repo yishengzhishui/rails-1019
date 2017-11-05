ccfff9b6f70e90072ffc1e95f4eeeed48291a8dc
wangxing <18912540921@163.com>
2017-11-05 19:46:08 +0800

    new and create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,29 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
